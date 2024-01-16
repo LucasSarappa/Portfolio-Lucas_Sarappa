5d7225d84a67e6dc870046115f53c0791988d8e9
Lucas Sarappa <lsarappa@frba.utn.edu.ar>
2024-01-16 18:57:54 -0300

    subo portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,200 @@
+export const Bio = {
+  name: "Lucas Sarappa",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/LucasSarappa",
+  resume:
+    "https://drive.google.com/file/d/1Cml0riWAK_5J-dLhKKbTuiTfyDBJ5vRL/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/lucassarappa/",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/",
+  facebook: "https://www.facebook.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image: "https://cdn.worldvectorlogo.com/logos/java.svg",
+      },
+      {
+        name: "Spring Boot",
+        image: "https://cdn.worldvectorlogo.com/logos/spring-3.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://cdn.worldvectorlogo.com/logos/fiverr-1.svg",
+    role: "Full Stack Developer",
+    company: "Freelance/Personal Proyects",
+    date: "Enero 2022 - Present",
+    desc: "Working on the creation of different projects, both personal and some freelance.",
+    skills: [
+      "Java",
+      "SpringBoot",
+      "React",
+      "JavaScript",
+    ],
+    doc: "",
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://pbs.twimg.com/profile_images/378800000468818939/6c6ec23e6d9534c519788e0e35bccadd_400x400.jpeg" ,
+    school: "National Technological University, Regional Buenos Aires",
+    date: "Ene 2021 - Present",
+    grade: "8.7",
+    desc: "I am currently studying Information Systems Engineering at the National Technological University. I am currently studying the 3rd year of my degree, with a total of 15 subjects approved out of 45",
+    degree: "Information Systems Engineering",
+  },
+
+  {
+    id: 1,
+    img: "https://pbs.twimg.com/profile_images/378800000468818939/6c6ec23e6d9534c519788e0e35bccadd_400x400.jpeg",
+    school: "National Technological University, Regional Buenos Aires",
+    date: "Ene 2021 - Present",
+    grade: "8.7",
+    desc: "I am 8 subjects away from obtaining the title of Systems Analyst offered by the National Technological University as an intermediate degree for the Systems Engineering degree.",
+    degree: "Systems Analyst",
+  },
+  {
+    id: 3,
+    img: "https://sicosinformatica.com.ar/wp-content/themes/sicos_template/img/img-web.png",
+    school: "Sicos Informatica",
+    date: "Sep 2023 - Dic 2023",
+    grade: "9",
+    desc: "Take the backend developer course, taught by Sicos Informatica. In it, Java and Docker concepts were seen.",
+    degree: "Java Backend Developer",
+  },
+  {
+    id: 4,
+    img: "https://www.ipm.edu.ar/assets/img/logos/logo.png",
+    school: "Insituto Politecnico Modelo",
+    date: "Mar 2014 - Dic 2019",
+    grade: "8",
+    desc: "During my secondary studies, I trained as a Computer Technician at the Model Polytechnic Institute.",
+    degree: "Computer Technician",
+  },
+];
+
+
+export const projects = [
+  {
+    id: 0,
+    title: "",
+    date: "",
+    description:
+      "",
+    image:
+      "",
+    tags: [
+      "",
+      "",
+      "",
+      "",
+
+    ],
+    category: "",
+    github: "",
+    webapp: "",
+  },
+  
+];
